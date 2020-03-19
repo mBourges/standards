2dd8a29223094ae00b0653e53aba05ca2c174745
Matt B <m.bourges@gmail.com>
2020-03-19 12:42:44 +0100

    chore: add support for Okta in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,3 +23,28 @@ exports.onCreateNode = ({ node, actions }) => {
     })
   }
 }
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions;
+
+  if (page.path.match(/^\/account/)) {
+    page.matchPath = "/account/*";
+    createPage(page)
+  }
+};
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    // Exclude Sign-In Widget from compilation path
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /okta-sign-in/,
+            use: loaders.null(),
+          }
+        ],
+      },
+    })
+  }
+};
